cb5513255479f101c31beb5b8d98424acb172737
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-05-04 21:00:28 +0900

    Initial Commit

---
+++ artKyu/artKyuApp.swift
@@ -0,0 +1,17 @@
+//
+//  artKyuApp.swift
+//  artKyu
+//
+//  Created by Hankyu Lee on 2022/05/04.
+//
+
+import SwiftUI
+
+@main
+struct artKyuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
